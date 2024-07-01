b8c6f33db27b17f2a315c32ff8699be21668b1e7
Rado <radonirina.maminiaina@gmail.com>
2024-07-01 19:35:59 +0300

    fix signed in

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -1,4 +1,8 @@
-import { OrganizationProfile, OrganizationSwitcher } from "@clerk/clerk-react";
+import {
+  OrganizationProfile,
+  OrganizationSwitcher,
+  SignedIn,
+} from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
 
 import styled from "styled-components";
@@ -18,16 +22,18 @@ const Wrapper = styled.div({
 export default function OrganizationPage() {
   return (
     <Wrapper>
-      <div className="p-5 border-b border-solid border-gray-200 flex justify-between gap-x-5">
-        <OrganizationSwitcher />
-        <Link
-          to="/organisation/create"
-          className="bg-gray-700 px-4 py-1 text-white cursor-pointer rounded-md"
-        >
-          New organization
-        </Link>
-      </div>
-      <OrganizationProfile />
+      <SignedIn>
+        <div className="p-5 border-b border-solid border-gray-200 flex justify-between gap-x-5">
+          <OrganizationSwitcher />
+          <Link
+            to="/organisation/create"
+            className="bg-gray-700 px-4 py-1 text-white cursor-pointer rounded-md"
+          >
+            New organization
+          </Link>
+        </div>
+        <OrganizationProfile />
+      </SignedIn>
     </Wrapper>
   );
 }
